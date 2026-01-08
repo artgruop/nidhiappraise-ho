d6b89bee4a39390a3be980812468c0ef4208e1b6
Art Group <artgroup1995@gmail.com>
2026-01-08 16:43:29 +0530

    db

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -1,6 +1,6 @@
 // firebaseConfig.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
-import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-database.js";
+import { getDatabase, ref, push, onValue, update, remove} from "https://www.gstatic.com/firebasejs/11.4.0/firebase-database.js";
 
 const appSetting = {
     databaseURL: "https://nidhi-appraise-default-rtdb.firebaseio.com/"
@@ -9,7 +9,7 @@ const appSetting = {
 const app = initializeApp(appSetting);
 const database = getDatabase(app);
 
-export { database, ref, push, onValue }; 
+export { database, ref, push, onValue, update, remove }; 
 
 
 /*
